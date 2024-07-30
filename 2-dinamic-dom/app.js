60007e04c4a69104d3c216cfc4ee829b02b406fa
afsaanchez04 <afsanchez04@gmail.com>
2024-07-30 13:18:36 -0500

    feat: maquetación creador pelotas

---
+++ 2-dinamic-dom/app.js
@@ -0,0 +1,5 @@
+const listaColores = document.querySelector('#listaColores')
+const btnCrear = document.querySelector('#btnCrear')
+const btnBorrar = document.querySelector('#btnBorrar')
+const main = document.querySelector('.main')
+
