799e9885e562100278df20714467842965801c68
afsaanchez04 <afsanchez04@gmail.com>
2024-07-31 13:27:54 -0500

    feat: crear-pelota

--- 2-dinamic-dom/app.js
+++ 2-dinamic-dom/app.js
@@ -2,4 +2,25 @@ const listaColores = document.querySelector('#listaColores')
 const btnCrear = document.querySelector('#btnCrear')
 const btnBorrar = document.querySelector('#btnBorrar')
 const main = document.querySelector('.main')
+let num = 1;
 
+function crear (){
+
+    let pelota = document.createElement('div')  
+    pelota.classList.add('pelota');
+    pelota.innerHTML = `
+        <div class="equis">x</div>
+        <span>${num++}</span>
+    ` 
+    pelota.style.backgroundColor = listaColores.value;
+
+    main.appendChild(pelota);
+}
+
+function borrarTodo(){
+    main.innerHTML = "";
+    num = 1;
+}
+
+btnCrear.addEventListener('click',crear);
+btnBorrar.addEventListener('click',borrarTodo)
